08ec4779387c1a5e3c0c337e35a8d48afc80bb9f
mohsin_development <mq0062783@gmail.com>
2025-08-12 14:50:37 +0530

    cors origin changes

--- app.js
+++ app.js
@@ -1,76 +1,20 @@
-// import express from "express";
-// import cors from "cors";
-// import contactRoutes from "./src/api/contact.js";
-
-// const app = express();
-
-// // app.use(cors());//local chalane k liye isko enable kro 
-// app.use(cors({
-//   origin: "https://mohsinqureshiportfolio.netlify.app",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   credentials: true
-// }));
-// app.use(express.json());
-
-
-// // API routes
-// app.use("/api/contact", contactRoutes);
-
-// export default app;
-
-// import express from "express";
-// import cors from "cors";
-// import contactRoutes from "./src/api/contact.js";
-
-// const app = express();
-
-// const allowedOrigins = [
-//   "https://mohsinqureshiportfolio.netlify.app"
-// ];
-
-// app.use(cors({
-//   origin: allowedOrigins,
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true
-// }));
-
-// app.options("*", cors({ origin: allowedOrigins })); // preflight ke liye
-
-// app.use(express.json());
-
-// // API routes
-// app.use("/api/contact", contactRoutes);
-
-// export default app;
-
-
-
-// src/api/contact.js
 import express from "express";
 import cors from "cors";
+import contactRoutes from "./src/api/contact.js";
 
-const router = express.Router();
+const app = express();
 
-const allowedOrigins = ["https://mohsinqureshiportfolio.netlify.app"];
-
-router.use(cors({
-  origin: allowedOrigins,
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+// app.use(cors());//local chalane k liye isko enable kro 
+app.use(cors({
+  origin: "https://mohsinqureshiportfolio.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+app.use(express.json());
 
-// Preflight handle
-router.options("*", cors({
-  origin: allowedOrigins,
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
 
-router.post("/", async (req, res) => {
-  res.json({ success: true, message: "Message sent" });
-});
+// API routes
+app.use("/api/contact", contactRoutes);
 
-export default router;
+export default app;
+
