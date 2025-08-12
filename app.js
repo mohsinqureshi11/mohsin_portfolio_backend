26b8cc4e28a6e5bc72be9a6fa450525c2664713a
mohsin_development <mq0062783@gmail.com>
2025-08-12 14:28:27 +0530

    cors origin changes

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ import contactRoutes from "./src/api/contact.js";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());//local chalane k liye isko enable kro 
+app.use(cors({
+  origin: "https://mohsinqureshiportfolio.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // API routes
