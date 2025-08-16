326629fe9a255898a0d2b565714207e17aeae4bf
mohsin_development <mq0062783@gmail.com>
2025-08-16 17:52:01 +0530

    backend entegrationsr

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ import contactRoutes from "./src/api/contact.js";
 const app = express();
 
 // app.use(cors());//local chalane k liye isko enable kro 
-app.use(cors({
-  origin: "https://mohsinqureshiportfolio.netlify.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 
