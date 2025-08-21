b42cd26d76980a370fe238b72e70f9fa68f69783
mohsin_development <mq0062783@gmail.com>
2025-08-21 14:28:46 +0530

    up to date

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+import http from "http";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config()
 import contactRoutes from "./src/api/contact.js";
 
+ 
 const app = express();
 
-// app.use(cors());//local chalane k liye isko enable kro 
 app.use(cors());
-app.use(express.json());
 
-
-// API routes
+app.use(express.json({limit:"200mb"}))   
+app.use(express.urlencoded({limit:"200mb",extended:true}))
+ 
 app.use("/api/contact", contactRoutes);
 
-export default app;
+const port= process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
+
+
+ 
 
